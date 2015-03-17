2bd50499c824f074a8dd04caded8319bfafcc494
SwissMiss <koyamaneko42@gmail.com>
2015-03-17 11:24:22 -0400

    it more imgs

---
+++ src/inferno/Grid.java
@@ -0,0 +1,12 @@
+package inferno;
+
+public class Grid {
+
+	private int x=16, y13;
+	
+	public Grid(int x, int y) {
+		//set grid size - default should be 16 x 13
+		
+	}	//end constructor
+	
+}	//end Grid
