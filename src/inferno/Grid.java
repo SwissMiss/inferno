b5da9134124efef09fa9111ed089ac88a2642f3c
SwissMiss <koyamaneko42@gmail.com>
2015-03-18 12:12:57 -0400

    mmm that you only meant well

--- src/inferno/Grid.java
+++ src/inferno/Grid.java
@@ -2,11 +2,13 @@ package inferno;
 
 public class Grid {
 
-	private int x=16, y13;
+	private int x=16, y=13;
 	
 	public Grid(int x, int y) {
 		//set grid size - default should be 16 x 13
-		
+		this.x=x; this.y=y;
 	}	//end constructor
+	
+	
 	
 }	//end Grid
